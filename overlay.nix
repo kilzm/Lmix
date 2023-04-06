5abdf572f51b5f733ff0e90534572aafc2cf1293
Kilian M <kilian02.markl@gmail.com>
2023-04-06 16:05:42 +0200

    add implicit import of overlays

--- overlay.nix
+++ overlay.nix
@@ -1,9 +1,6 @@
 final: prev:
   let
-    overlays = [
-      (import ./overlays/misc-overlay.nix)
-      (import ./overlays/julia-overlay.nix)
-    ];
+    overlays = map import (prev.lib.filesystem.listFilesRecursive ./overlays);
     overlay = with prev.lib; foldl' composeExtensions (_: _: {}) overlays;
   in
      overlay final prev
