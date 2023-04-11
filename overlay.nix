f102c246a5a2559ecc15a0c4febc8a9116d9b98d
Kilian M <kilian02.markl@gmail.com>
2023-04-11 13:16:29 +0200

    refactor overrides

--- overlay.nix
+++ overlay.nix
@@ -4,7 +4,7 @@ final: prev: rec {
   hello_2_12_1 = prev.hello.overrideAttrs (old: rec {
     version = "2.12.1";
     src = prev.fetchurl {
-      url = "mirror://gnu/hello/hello-${version}.tar.gz";
+      url = "mirror://gnu/${old.pname}/${old.pname}-${version}.tar.gz";
       sha256 = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
     };
   });
@@ -12,7 +12,7 @@ final: prev: rec {
   hello_2_9 = prev.hello.overrideAttrs (old: rec {
     version = "2.9";
     src = prev.fetchurl {
-      url = "mirror://gnu/hello/hello-${version}.tar.gz";
+      url = "mirror://gnu/${old.pname}/${old.pname}-${version}.tar.gz";
       sha256 = "sha256-7Lt6IhQZbFf/k0CqcUWOFVmr049tjRaWZoRpNd8ZHqc=";
     };
   });
@@ -24,11 +24,10 @@ final: prev: rec {
 
   # openmpi
   openmpi_4_1_5 = prev.openmpi.overrideAttrs (old: rec { 
-    pname = "openmpi";
     version = "4.1.5";
 
     src = with prev.lib.versions; prev.fetchurl {
-      url = "https://www.open-mpi.org/software/ompi/v${major version}.${minor version}/downloads/${pname}-${version}.tar.bz2";
+      url = "https://www.open-mpi.org/software/ompi/v${major version}.${minor version}/downloads/${old.pname}-${version}.tar.bz2";
       sha256 = "sha256-pkCYa8JXOJ3TeYhv2uYmTIz6VryYtxzjrj372M5h2+M=";
     };
   });
@@ -41,19 +40,17 @@ final: prev: rec {
   osu-micro-benchmarks_5_6_2 = osu-micro-benchmarks;
 
   osu-micro-benchmarks_5_4 = osu-micro-benchmarks.overrideAttrs (old: rec {
-    pname = "osu-micro-benchmarks";
     version = "5.4";
     src = prev.fetchurl {
-      url = "mvapich.cse.ohio-state.edu/download/mvapich/${pname}-${version}.tar.gz";
+      url = "mvapich.cse.ohio-state.edu/download/mvapich/${old.pname}-${version}.tar.gz";
       sha256 = "sha256-4cp2LhOgcgWlm1mthehc4Pgmtw92/VVc5VaO+x8qjzM=";
     };
   });
 
   osu-micro-benchmarks_6_1 = osu-micro-benchmarks.overrideAttrs (old: rec {
-    pname = "osu-micro-benchmarks";
     version = "6.1";
     src = prev.fetchurl {
-      url = "mvapich.cse.ohio-state.edu/download/mvapich/${pname}-${version}.tar.gz";
+      url = "mvapich.cse.ohio-state.edu/download/mvapich/${old.pname}-${version}.tar.gz";
       sha256 = "sha256-7MztyGgmT3XbTZUpr3kAVBmid1ETx/ro9OSoQ0Ni5Kc=";
     };
   });
