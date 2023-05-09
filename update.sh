e97b65ff1df2abe876aa085a2ffde40d60378891
Kilian M <kilian02.markl@gmail.com>
2023-05-09 17:04:04 +0200

    fix update script disabling sandboxing only for onapi

--- update.sh
+++ update.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 
-sandboxTrue() {
-  sed -i 's/sandbox = false/sandbox = true/g' /etc/nix/nix.conf
-}
-
-sandboxFalse() {
-  sed -i 's/sandbox = true/sandbox = false/g' /etc/nix/nix.conf
-}
-
 # requires MODULEPATH set to /opt/modulefiles/modules-nix/modules
 modpath=/opt/modulefiles
 rm -f $modpath/*
 
-sandboxTrue
 nix profile install .#modules --profile $modpath/modules-nix
 
-sandboxFalse
-export NIXPKGS_ALLOW_UNFREE=1
-export NIXPKGS_ALLOW_INSECURE=1
+export NIXPKGS_ALLOW_UNFREE=1 # intel software uses unfree license (EULA)
+export NIXPKGS_ALLOW_INSECURE=1 # oneapi needs qt515.full to build which has insecure component qt-webtoolkit
+ # oneapi only builds with sandboxing disabled
+ # --impure is needed so nix can read the environment variables for unfree and insecure
+nix build --impure .#intel-oneapi_2022_2_0 --no-sandbox
 nix profile install --impure .#modules-intel --profile $modpath/modules-nix
 export NIXPKGS_ALLOW_UNFREE=0
 export NIXPKGS_ALLOW_INSECURE=0
-sandboxTrue
