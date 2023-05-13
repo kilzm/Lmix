2b82b1e3ca1f658b2943740aa31d796dea97b92a
Kilian M <kilian02.markl@gmail.com>
2023-05-13 03:28:12 +0200

    fix update script creating unwanted result symlink

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ export NIXPKGS_ALLOW_UNFREE=1 # intel software uses unfree license (EULA)
 export NIXPKGS_ALLOW_INSECURE=1 # oneapi needs qt515.full to build which has insecure component qt-webtoolkit
  # oneapi only builds with sandboxing disabled
  # --impure is needed so nix can read the environment variables for unfree and insecure
-nix build --impure .#intel-oneapi_2022_2_0 --no-sandbox
+nix build --impure .#intel-oneapi_2022_2_0 --no-sandbox --no-link
 nix profile install --impure .#modules-intel --profile $modpath/modules-nix
 export NIXPKGS_ALLOW_UNFREE=0
 export NIXPKGS_ALLOW_INSECURE=0
