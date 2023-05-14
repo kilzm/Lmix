37ed57bb60a6427784a5ea7e90037e770a1b173b
Kilian M <kilian02.markl@gmail.com>
2023-05-14 14:17:19 +0200

    use optionalString for package variables

--- modules/default.nix
+++ modules/default.nix
@@ -12,10 +12,11 @@
 }:
 
 with lib;
+with lib.strings;
 
 assert (builtins.typeOf compilerVer) == "int";
 assert compiler == ""
-  || compiler == "gcc" && 1 <= compilerVer && compilerVer <= 12
+  || compiler == "gcc" && 7 <= compilerVer && compilerVer <= 13
   || compiler == "intel" && 19 <= compilerVer && compilerVer <= 23;
 
 stdenv.mkDerivation rec {
@@ -45,32 +46,41 @@ stdenv.mkDerivation rec {
 
   pacName =
     if cpacName == ""
-    then builtins.replaceStrings [ "-" ] [ "_" ] (lib.strings.toUpper pkgName)
+    then builtins.replaceStrings [ "-" ] [ "_" ] (toUpper pkgName)
     else cpacName;
 
   # from lrz documentation
   PAC_BASE = "${pkg}";
-  PAC_LIBDIR = if hasLibs then "${PAC_BASE}/lib" else "";
-  PAC_LIB =
-    let path = "${PAC_LIBDIR}/lib${libName}.a";
-    in if builtins.pathExists path then path else "";
+
+  PAC_LIBDIR = optionalString hasLibs "${PAC_BASE}/lib";
+
+  PAC_LIB = 
+    let path = "${PAC_LIBDIR}/lib${libName}.a"; 
+    in optionalString (builtins.pathExists path) path;
+
   PAC_SHLIB =
     let path = "${PAC_LIBDIR}/lib${libName}.so";
-    in if builtins.pathExists path then "-L${PAC_LIBDIR} -l${libName}" else "";
+    in optionalString (builtins.pathExists path) "-L${PAC_LIBDIR} -l${libName}";
+
   PAC_PTHREADS_LIB =
     let path = "${PAC_LIBDIR}/lib${libName}_threads.a";
-    in if builtins.pathExists path then path else "";
+    in optionalString (builtins.pathExists path) path;
+
   PAC_PTHREADS_SHLIB =
     let path = "${PAC_LIBDIR}/lib${libName}_threads.so";
-    in if builtins.pathExists path then path else "";
+    in optionalString (builtins.pathExists path) "-L${PAC_LIBDIR} -l${libName}_threads";
+
   PAC_MPI_LIB =
     let path = "${PAC_LIBDIR}/lib${libName}_mpi.a";
-    in if builtins.pathExists path then path else "";
+    in optionalString (builtins.pathExists path) path;
+
   PAC_MPI_SHLIB =
     let path = "${PAC_LIBDIR}/lib${libName}_mpi.so";
-    in if builtins.pathExists path then path else "";
-  PAC_INC = if hasIncs then "-I${PAC_BASE}/include" else "";
-  PAC_BIN = if hasBin then "${PAC_BASE}/bin" else "";
+    in optionalString (builtins.pathExists path) "${PAC_LIBDIR} -l${libName}_mpi";
+
+  PAC_INC = optionalString hasIncs "-I${PAC_BASE}/include";
+
+  PAC_BIN = optionalString hasBin "${PAC_BASE}/bin";
 
   inherit extraPkgVariables extraEnvVariables;
 }
