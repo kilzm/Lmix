42906be2b0d33d9d640191cdb4f398bc1a4b76c9
Kilian M <kilian02.markl@gmail.com>
2023-05-28 23:18:48 +0200

    fix module configurations

--- modules/intel/oneapi-mpi/default.nix
+++ modules/intel/oneapi-mpi/default.nix
@@ -1,28 +1,23 @@
 { lib, pkg, cc, ... }:
 with lib;
-with lib.strings;
 let
-  # TODO
-  # isGCC = compiler == "gcc";
-  # isIntel = compiler == "intel";
-  # cc = ""
-  #   + optionalString isGCC "gcc"
-  #   + optionalString isIntel "icc";
-  # cxx = ""
-  #   + optionalString isGCC "g++"
-  #   + optionalString isIntel "icpc";
+  isGCC = strings.hasPrefix "gcc" cc;
+  isIntel = strings.hasPrefix "intel" cc;
+  CC = if isGCC then "gcc" else "icc";
+  CXX = if isGCC then "g++" else "icpc";
+  module = if isGCC then let ver = strings.removePrefix "gcc" cc; in "gcc/${ver}"
+  else let ver = strings.removePrefix "intel" cc; in "intel-oneapi-compilers/${ver}";
 in
 {
   libName = "mpi";
   libPath = "lib/release";
 
-  # dependencies = [ ]
-  #   ++ optional isGCC "gcc/${builtins.toString compilerVer}";
+  dependencies = [ module ];
 
-  # extraEnvVariables = builtins.toJSON {
-  #   I_MPI_CC = "${cc}";
-  #   I_MPI_CXX = "${cxx}";
-  # };
+  extraEnvVariables = builtins.toJSON {
+    I_MPI_CC = "${CC}";
+    I_MPI_CXX = "${CXX}";
+  };
 
   customScriptPath = "${pkg}/env/vars.sh";
 }
--- modules/python/default.nix
+++ modules/python/default.nix
@@ -1,9 +1,9 @@
 { lib, pkg, ... }:
-{
+rec {
   libName = "python${lib.versions.majorMinor pkg.version}";
   pkgName = "python";
 
-  # incPath = "include/${libName}";
+  incPath = "include/${libName}";
 
   extraPkgVariables = builtins.toJSON {
     WWW = "https://doku.lrz.de/display/PUBLIC/Python+for+HPC";
