52f0b7480e0f33faeb7577852f302af7e02f6f53
Kilian Markl <kilian02.markl@gmail.com>
2023-06-30 00:47:20 +0200

    improve adios2 derivation

--- pkgs/ADIOS/default.nix
+++ pkgs/ADIOS/default.nix
@@ -4,7 +4,6 @@
 , cmake
 , pkg-config
 , fortran
-, mpi
 , bzip2
 , libfabric
 , rdma-core
@@ -23,10 +22,10 @@
 , mgard
 , protobuf
 , sz
-, python311
-, python311Packages
 , zfp
 , zstd
+, python311
+, python311Packages
 }:
 
 with lib;
@@ -41,50 +40,43 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-CRpmhXYa3WSaeNOc//4A6dcQrUIyWpogUggkqng0JYA=";
   };
 
-  preConfigure = ''
-    export FC=$FC_FOR_TARGET
-  '';
-
   cmakeFlags = [
     "-DPYTHON_EXECUTABLE=${python311}/bin/python3"
     "-DADIOS2_INSTALL_GENERATE_CONFIG=OFF"
     "-DADIOS2_USE_DataSpaces=OFF"
     "-DADIOS2_USE_IME=OFF"
     "-DCMAKE_DISABLE_FIND_PACKAGE_CrayDRC=TRUE"
+    "-DCMAKE_DISABLE_FIND_PACKAGE_DAOS=TRUE"
   ];
 
   buildInputs = [
-    fortran
+    hdf5.mpi
+    bzip2
+    libfabric
     rdma-core
     doxygen
     graphviz
     bison
     flex
-    hdf5.mpi
-    hdf5
-    mgard
-    zstd
-    protobuf
-    bzip2
     perl
-    libfabric
-    zfp
     ucx
+    zeromq
+    hdf5
+    libffi
     libsodium
     libpng
-    libffi
-    zeromq
-    sz
     c-blosc
+    mgard
+    protobuf
+    sz
+    zfp
+    zstd
     python311
     python311Packages.numpy
     python311Packages.mpi4py
   ];
 
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-  ];
+  nativeBuildInputs = [ cmake pkg-config fortran ];
 
   passthru = {
     mpi = hdf5.mpi;
