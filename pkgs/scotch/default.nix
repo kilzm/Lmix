0b7ce0b56016d0bc8fac887ad97095ad095f411c
Kilian Markl <kilian02.markl@gmail.com>
2023-08-18 00:14:01 +0200

    add scotch

---
+++ pkgs/scotch/default.nix
@@ -0,0 +1,56 @@
+{ stdenv
+, lib
+, fetchFromGitLab
+, cmake
+, fortran
+, mpi
+, bzip2
+, zlib
+, xz
+, bison
+, flex
+}:
+
+stdenv.mkDerivation rec {
+  pname = "scotch";
+  version = "7.0.3";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.inria.fr";
+    owner = "scotch";
+    repo = "scotch";
+    rev = "v${version}";
+    sha256 = "sha256-wrOH0+DDzJ6utJYRSZWu2td1dTWJCzgfpNs4/QoiOk8=";
+  };
+
+  preConfigure = ''
+    export I_MPI_CC=$CC
+    export I_MPI_ROOT=${mpi}
+  '';
+
+  nativeBuildInputs = [ cmake fortran ];
+
+  cmakeFlags = [
+    "-DBUILD_SHARED_LIBS=ON"
+  ];
+
+  buildInputs = [
+    mpi
+    bzip2
+    zlib
+    xz
+    bison
+    flex
+  ];
+
+  passthru = {
+    inherit mpi;
+  };
+
+  meta = with lib; {
+    description = "Package for graph and mesh/hypergraph partitioning, graph clustering, and sparse matrix ordering";
+    homepage = "https://www.labri.fr/perso/pelegrin/scotch/";
+    license = licenses.cecill-c;
+    platform = platforms.unix;
+  };
+}
