5bf67a7d7186bdb00e1e42fdf58a455874d8ac5d
Kilian Markl <kilian02.markl@gmail.com>
2023-08-28 18:16:38 +0200

    use nix profile remove

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# requires MODULEPATH set to /opt/modulefiles/modules-nix/modules
+# requires MODULEPATH set to /opt/modulefiles/modules-nix/${pkgs-ver}
 modpath=/opt/modulefiles
-rm -f $modpath/modules-nix*
 
-modSuffixes=("-nixpkgs" "")
+if [[ -d "${modpath}/modules_nix" ]]; then
+  nix profile remove --profile ${modpath}/modules_nix '.*'
+fi
 
-for s in "${modSuffixes[@]}"; do
-  nix profile install .#_modules$s --profile $modpath/modules-nix
-done
+nix profile install .#_modules .#_modules-nixpkgs --profile $modpath/modules_nix
